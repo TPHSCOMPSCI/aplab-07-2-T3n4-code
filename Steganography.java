f66186d9cfea0025712bfcb3c37457373d841fae
T3n4-code <81334524+T3n4-code@users.noreply.github.com>
2025-05-02 12:07:28 -0700

    APlab 7.2

--- Steganography.java
+++ Steganography.java
@@ -1,20 +1,24 @@
-import java.awt.Color; 
-    /**
-    * Clear the lower (rightmost) two bits in a pixel.
-    */
-    public static void clearLow( Pixel p ){ 
-      p.setRed(p.getRed() & 0b11111100);
-        p.setGreen(p.getGreen() & 0b11111100);
-        p.setBlue(p.getBlue() & 0b11111100);
-    }
-      public static Picture testClearLow(Picture fromPic){
-        return new Picture();
-    }
+  import java.awt.Color; 
+    //** * Clear the lower (rightmost) two bits in a pixel. */ 
+    
     public class Steganography{ 
-    public static void main(String[]args) {
+
+    public static void main(String[] args) {
     Picture beach = new Picture ("beach.jpg");
     beach.explore();
     Picture copy = testClearLow(beach);
     copy.explore(); 
     }
+    public static void clearLow( Pixel p ){ 
+      p.setRed(p.getRed() & 0b11111100);
+        p.setGreen(p.getGreen() & 0b11111100);
+        p.setBlue(p.getBlue() & 0b11111100);
+    }
+
+    public static Picture testClearLow(Picture fromPic){
+      return new Picture();
+    }
+
+
   }
+  
